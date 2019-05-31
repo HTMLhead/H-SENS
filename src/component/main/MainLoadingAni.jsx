bcfb2d1a5d88257c7bb328824b3ea4b697a0a7bd
HTMLhead <mrdoo309@gmail.com>
2019-05-31 14:03:49 +0900

    Feat: 메인 로딩애니 수정

--- src/component/main/MainLoadingAni.jsx
+++ src/component/main/MainLoadingAni.jsx
@@ -5,6 +5,8 @@ const MainLoadingAni = styled.div`
   margin: auto;
   height: 50vh;
   background-image: url('./image/mainLoading.gif');
+  background-size: contain;
+  background-repeat: no-repeat;
 `;
 
 export default MainLoadingAni;
