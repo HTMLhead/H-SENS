87a0a25e87fe24f602436cb3aa1d8f44610af675
HTMLhead <mrdoo309@gmail.com>
2019-05-31 12:12:30 +0900

    Refactor: userLink dropdown 버튼으로 수정

--- src/component/header/UserLinkBtn.jsx
+++ src/component/header/UserLinkBtn.jsx
@@ -1,17 +1,19 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { Dropdown } from 'semantic-ui-react';
 
-const UserLinkStyled = styled(Link)`
-  display:block;
-  background-image: url("${props =>
-    props.userimage || `${process.env.REACT_APP_CLIENT_URL}/image/blank.png`}");
-  background-size: contain;
-  background-repeat: no-repeat;
-  background-position: center center;
-  border: 1px solid #011627;
-  border-radius: 50%;
-  width: 4.5rem;
-  height: 4.5rem;
+const UserDropdown = styled(Dropdown)`
+  &&& {
+    display:block;
+    background-image: url("${props =>
+      props.userimage || `${process.env.REACT_APP_CLIENT_URL}/image/blank.png`}");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center center;
+    border: 1px solid #011627;
+    border-radius: 50%;
+    width: 4.5rem;
+    height: 4.5rem;
+  }
 `;
-
-export default UserLinkStyled;
+export default UserDropdown;
